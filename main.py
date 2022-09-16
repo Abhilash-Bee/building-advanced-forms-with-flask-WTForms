d0db21a18b39c2bb2104e459bf6c892fb49807ef
Abhilash-Bee <abhilashjanarrdhan@gmail.com>
2022-09-16 14:28:47 +0530

    Building Advanced Forms with Flask - WTForms

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Email, InputRequired, Length
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+app.secret_key = "beeliki"
+
+
+class LoginForm(FlaskForm):
+    email = StringField(label='Email', validators=[InputRequired(), Email()])
+    password = PasswordField(label='Password', validators=[InputRequired(), Length(min=8)])
+    submit = SubmitField(label='Log In')
+
+
+@app.route("/")
+def home():
+    return render_template('index.html')
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "iambee8498@gmail.com" and login_form.password.data == "beeeliki":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
+    return render_template('login.html', login_form=login_form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
